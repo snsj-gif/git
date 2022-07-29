74fd9b4e716fb4867f399f3a6eb26997acc44c28
baiyuyang <baiyuyang@qdcares.cn>
2022-07-29 19:20:20 +0800

    first commit

--- src/main/java/com/example/git/GitApplication.java
+++ src/main/java/com/example/git/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("helloGit2");
+        System.out.println("helloGit hot-fix");
 
     }
 
